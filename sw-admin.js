137c0bca6a10620c7f0d55684f3a4ff9a0acddea
projetominervaetevm <projetominerva.etevm@gmail.com>
2026-05-31 08:08:25 -0300

    Create sw-admin.js

---
+++ sw-admin.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'minerva-supervisor-cache-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
